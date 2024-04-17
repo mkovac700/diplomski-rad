93af3078ef0500cfb7b0dd0eed8e44f3fdc8b1a7
Marijan Kovač <mkovac@student.foi.hr>
2024-04-17 23:11:07 +0200

    Implementiran prikaz razine zvuka uz Qt Multimedia
    - Postignuta ista funkcionalnost kao i s vanjskom bibliotekom PortAudio

--- Software/audio-visualizer/mainwindow.cpp
+++ Software/audio-visualizer/mainwindow.cpp
@@ -4,18 +4,34 @@
 #include <QAudioDevice>
 #include <QVariant>
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
+    if(m_devices->audioInputs().isEmpty()) QMessageBox::critical(this,"Greška","Nema dostupnih uređaja!");
     loadDevices();
+    // initializeAudio(QMediaDevices::defaultAudioInput());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::set_level_l(qreal value)
+{
+    int level = qRound(qreal(ui->progressBar_Vol_L->maximum() * value));
+    ui->progressBar_Vol_L->setValue(level);
+}
+
+void MainWindow::set_level_r(qreal value)
+{
+    int level = qRound(qreal(ui->progressBar_Vol_R->maximum() * value));
+    ui->progressBar_Vol_R->setValue(level);
 }
 
 void MainWindow::loadDevices()
@@ -50,3 +66,65 @@ void MainWindow::loadDevices()
         }
     }
 }
+
+void MainWindow::initializeAudio(const QAudioDevice &inputDeviceInfo)
+{
+    QAudioFormat format;
+    format.setSampleRate(m_sample_rate); //8000
+    format.setChannelCount(2);
+    format.setSampleFormat(QAudioFormat::Int16);
+
+    m_audioListener.reset(new AudioListener(format));
+
+    connect(m_audioListener.data(), &AudioListener::level_l_Changed, this, &MainWindow::set_level_l);
+    connect(m_audioListener.data(), &AudioListener::level_r_Changed, this, &MainWindow::set_level_r);
+
+    m_audioInput.reset(new QAudioSource(inputDeviceInfo, format));
+
+    m_audioListener->start();
+    m_audioInput->start(m_audioListener.data()); //-> default, pull mode (moguce je implementirati i push mode -> bolja varijanta za real-time)
+}
+
+void MainWindow::on_pushButton_StartStop_clicked()
+{
+    if(m_audioInput.isNull()){ //prvi put -> jos nije inicijaliziran pointer m_audioInput
+        ui->pushButton_StartStop->setText("Zaustavi stream");
+        initializeAudio(ui->comboBox_AudioIn->currentData().value<QAudioDevice>());
+        return;
+    }
+
+    switch (m_audioInput->state()) {
+    case QtAudio::StoppedState:
+        ui->pushButton_StartStop->setText("Zaustavi stream");
+        initializeAudio(ui->comboBox_AudioIn->currentData().value<QAudioDevice>());
+        break;
+    case QtAudio::ActiveState:
+        ui->pushButton_StartStop->setText("Započni stream");
+        m_audioListener->stop();
+        m_audioInput->stop();
+        m_audioInput->disconnect(this);
+
+        ui->progressBar_Vol_L->setValue(0);
+        ui->progressBar_Vol_R->setValue(0);
+    default:
+        break;
+    }
+}
+
+
+void MainWindow::on_comboBox_SampleRate_currentTextChanged(const QString &text)
+{
+    int value = text.toInt();
+
+    if(value) m_sample_rate = value;
+}
+
+
+void MainWindow::on_pushButton_OsvjeziUredaje_clicked()
+{
+    ui->comboBox_AudioIn->clear();
+    ui->textEdit_Uredaji->clear();
+
+    loadDevices();
+}
+
